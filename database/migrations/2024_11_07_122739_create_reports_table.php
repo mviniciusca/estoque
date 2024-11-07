86d52abb68fbe8c9e408b08f4c325463e03f50af
Marcos Coelho <mviniciusca@gmail.com>
2024-11-07 09:45:51 -0300

    wip

--- database/migrations/2024_11_07_122739_create_reports_table.php
+++ database/migrations/2024_11_07_122739_create_reports_table.php
@@ -14,6 +14,8 @@ return new class () extends Migration {
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->integer('minimus')->default(3);
+            $table->boolean('is_dispatch')->default(false);
             $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignIdFor(Stock::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
