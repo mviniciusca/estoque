cb18bf2d0db2e759a47dcaf87c46dd99413510f5
Marcos Coelho <mviniciusca@gmail.com>
2024-11-07 09:41:54 -0300

    wip

--- database/migrations/2024_11_07_122630_create_stocks_table.php
+++ database/migrations/2024_11_07_122630_create_stocks_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
+            $table->integer('quantity')->default(0);
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
