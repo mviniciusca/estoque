486ed270f91d4fddd0e2c1207b0e0aef2770d427
Marcos Coelho <mviniciusca@gmail.com>
2024-11-07 09:51:57 -0300

    wip

--- database/migrations/2024_11_07_123039_create_locations_table.php
+++ database/migrations/2024_11_07_123039_create_locations_table.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Product;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
+            $table->string('section');
+            $table->string('geocode');
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
