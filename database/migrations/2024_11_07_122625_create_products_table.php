440b8c0d1b58afc1a051b4662fe6124dfb180c0c
Marcos Coelho <mviniciusca@gmail.com>
2024-11-07 09:39:57 -0300

    wip

--- database/migrations/2024_11_07_122625_create_products_table.php
+++ database/migrations/2024_11_07_122625_create_products_table.php
@@ -14,6 +14,7 @@ return new class () extends Migration {
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->boolean('is_active')->default(true);
             $table->string('name');
             $table->string('sku')->unique();
             $table->foreignIdFor(Stock::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
