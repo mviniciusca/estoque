2fe867f14233a46955c7772533c64a4f1e626a45
Marcos Coelho <mviniciusca@gmail.com>
2024-11-07 10:11:56 -0300

    wip

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Stock;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function stock()
+    {
+        $this->belongsTo(Stock::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- database/migrations/2024_11_05_122625_create_products_table.php
+++ database/migrations/2024_11_05_122625_create_products_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Category;
-use App\Models\Stock;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -17,7 +16,6 @@ return new class () extends Migration {
             $table->boolean('is_active')->default(true);
             $table->string('name');
             $table->string('sku')->unique();
-            $table->foreignIdFor(Stock::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
