4e88ecbd895fd6a9650a791013dfb5369e534669
Marcos Coelho <mviniciusca@gmail.com>
2024-11-07 13:16:05 -0300

    wip

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,19 +3,22 @@
 namespace App\Models;
 
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Location extends Model
 {
     /** @use HasFactory<\Database\Factories\LocationFactory> */
     use HasFactory;
+
     protected $guarded = [];
 
-
-
+    /**
+     * Summary of product
+     * @return BelongsTo
+     */
     public function product()
     {
         return $this->belongsTo(Product::class);
