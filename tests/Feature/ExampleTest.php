e01948fcd3c151341dd13a10ddf99368bf370087
Marcos Coelho <mviniciusca@gmail.com>
2024-11-08 10:35:29 -0300

    wip

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -8,12 +8,12 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * @test
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function it_should_show_error_on_server()
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertNotFound();
     }
 }
