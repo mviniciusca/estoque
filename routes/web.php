cd993954efff5e42479158e7ca186ea919747a8d
Marcos Coelho <mviniciusca@gmail.com>
2024-11-08 03:30:11 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect(route('web.login'));
-});
 Route::get('/admin/login', function () {
     Auth::loginUsingId(User::first()->id);
+
     return redirect(route('filament.admin.pages.dashboard'));
 })->name('filament.admin.auth.login');
