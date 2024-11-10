79fa6067871ceff9144ee1c5a588265ff94e8537
Marcos Coelho <mviniciusca@gmail.com>
2024-11-10 06:44:19 -0300

    wip

--- app/Filament/Resources/ReportResource/Pages/ListReports.php
+++ app/Filament/Resources/ReportResource/Pages/ListReports.php
@@ -15,7 +15,7 @@ class ListReports extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            //  Actions\CreateAction::make()->icon('heroicon-o-document')->label(__('Novo Relatório')),
+            //
         ];
     }
 }
