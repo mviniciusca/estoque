e4990f274f8af163ca401e6054e6f3374c1631e1
Marcos Coelho <mviniciusca@gmail.com>
2024-11-11 22:17:40 -0300

    wip

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Contracts\Support\Htmlable;
 
 class EditProduct extends EditRecord
 {
     protected static string $resource = ProductResource::class;
+
+    public function getTitle(): string | Htmlable
+    {
+        return __('Editar Produto ');
+    }
 
     protected function getHeaderActions(): array
     {
