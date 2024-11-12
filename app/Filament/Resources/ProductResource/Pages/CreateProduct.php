043acce81f66a6483a0b59a6a52c9ba0b2e836e2
Marcos Coelho <mviniciusca@gmail.com>
2024-11-11 22:17:59 -0300

    wip

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -5,8 +5,14 @@ namespace App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Contracts\Support\Htmlable;
 
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
+
+    public function getTitle(): string | Htmlable
+    {
+        return __('Criar Produto ');
+    }
 }
